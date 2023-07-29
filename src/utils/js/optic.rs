009ee05f3a692bc7073917ff1a8fdbb940c032ee
Daniel Slapman <danslapman@gmail.com>
2023-07-29 17:32:28 +0200

    `get_all` implementation

--- src/utils/js/optic.rs
+++ src/utils/js/optic.rs
@@ -1,6 +1,6 @@
-use std::fmt::{Display, Formatter};
 use serde_json::json;
 use serde_json::Value;
+use std::fmt::{Display, Formatter};
 
 #[derive(Clone, PartialEq)]
 pub enum PathPart {
@@ -14,7 +14,7 @@ impl Display for PathPart {
         match self {
             PathPart::Field(str) => write!(f, "{}", str),
             PathPart::Index(idx) => write!(f, "[{}]", idx),
-            PathPart::Traverse => write!(f, "$")
+            PathPart::Traverse => write!(f, "$"),
         }
     }
 }
@@ -47,7 +47,15 @@ impl JsonOptic {
 
 impl Display for JsonOptic {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "@->{}", self.json_path.iter().map(|part| part.to_string()).collect::<Vec<_>>().join("."))
+        write!(
+            f,
+            "@->{}",
+            self.json_path
+                .iter()
+                .map(|part| part.to_string())
+                .collect::<Vec<_>>()
+                .join(".")
+        )
     }
 }
 
@@ -67,16 +75,24 @@ trait ValueExt {
     fn set(&mut self, optic: &JsonOptic, v: &Value);
     fn set_opt(&mut self, optic: &JsonOptic, v: Option<&Value>);
     fn prune(&mut self, optic: &JsonOptic);
+    fn get_all(&self, optic: &JsonOptic) -> Vec<&Value>;
     fn validate(&self, optic: &JsonOptic) -> bool;
 }
 
 impl ValueExt for Value {
     fn set(&mut self, optic: &JsonOptic, v: &Value) {
-        let init: Box<dyn Fn(&mut Value)> = Box::new(|arg: &mut Value| {*arg = v.clone()});
+        let init: Box<dyn Fn(&mut Value)> = Box::new(|arg: &mut Value| *arg = v.clone());
 
         let modify_fn = optic.json_path.iter().rfold(init, |acc, el| {
             Box::new(move |arg: &mut Value| {
-                arg.modify_part_in_place(el, |v_ref| { acc(v_ref); ()}, || Value::Null);
+                arg.modify_part_in_place(
+                    el,
+                    |v_ref| {
+                        acc(v_ref);
+                        ()
+                    },
+                    || Value::Null,
+                );
             })
         });
 
@@ -86,7 +102,7 @@ impl ValueExt for Value {
     fn set_opt(&mut self, optic: &JsonOptic, v: Option<&Value>) {
         match v {
             Some(value) => self.set(optic, value),
-            None => self.prune(optic)
+            None => self.prune(optic),
         }
     }
 
@@ -95,30 +111,61 @@ impl ValueExt for Value {
             let init: Box<dyn Fn(&mut Value)> = match &optic.json_path[optic.json_path.len() - 1] {
                 PathPart::Field(field_name) => Box::new(|v: &mut Value| v.remove_field(field_name)),
                 PathPart::Index(index) => Box::new(|v: &mut Value| v.remove_at_index(*index)),
-                PathPart::Traverse => Box::new(|v: &mut Value| if v.is_array() { *v = Value::Null })
+                PathPart::Traverse => Box::new(|v: &mut Value| {
+                    if v.is_array() {
+                        *v = Value::Null
+                    }
+                }),
             };
 
-            let modify_fn = optic.json_path[0..(optic.json_path.len() - 1)].iter().rfold(init, |acc, el| {
-                Box::new(move |arg: &mut Value| {
-                    arg.modify_part_in_place(el, |v_ref| { acc(v_ref); ()}, || Value::Null)
-                })
-            });
+            let modify_fn = optic.json_path[0..(optic.json_path.len() - 1)]
+                .iter()
+                .rfold(init, |acc, el| {
+                    Box::new(move |arg: &mut Value| {
+                        arg.modify_part_in_place(
+                            el,
+                            |v_ref| {
+                                acc(v_ref);
+                                ()
+                            },
+                            || Value::Null,
+                        )
+                    })
+                });
 
             let _ = modify_fn(self);
         }
     }
 
+    fn get_all(&self, optic: &JsonOptic) -> Vec<&Value> {
+        optic.json_path.iter().fold(vec![self], |acc, el| match el {
+            PathPart::Field(name) => acc.iter().flat_map(|vx| vx.field(name)).collect::<Vec<_>>(),
+            PathPart::Index(index) => acc.iter().flat_map(|vx| vx.at_index(*index)).collect::<Vec<_>>(),
+            PathPart::Traverse => acc
+                .iter()
+                .flat_map(|vx| vx.as_array().map(|v| v.iter().collect::<Vec<_>>()).unwrap_or(vec![]))
+                .collect::<Vec<_>>(),
+        })
+    }
 
     fn validate(&self, optic: &JsonOptic) -> bool {
-        if optic.json_path.iter().all(|el| *el == PathPart::Traverse) {self.is_array()}
-        else {
-            !optic.json_path.iter().fold(vec![self], |acc, el| acc.iter().filter(|j| j.verify(el)).flat_map(|j| {
-                match el {
-                    PathPart::Field(field_name) => j.field(field_name).into_iter().collect::<Vec<_>>(),
-                    PathPart::Index(index) => j.at_index(*index).into_iter().collect::<Vec<_>>(),
-                    PathPart::Traverse => j.as_array().map(|v| v.iter().collect::<Vec<_>>()).unwrap_or(vec![])
-                }
-            }).collect()).is_empty()
+        if optic.json_path.iter().all(|el| *el == PathPart::Traverse) {
+            self.is_array()
+        } else {
+            !optic
+                .json_path
+                .iter()
+                .fold(vec![self], |acc, el| {
+                    acc.iter()
+                        .filter(|j| j.verify(el))
+                        .flat_map(|j| match el {
+                            PathPart::Field(field_name) => j.field(field_name).into_iter().collect::<Vec<_>>(),
+                            PathPart::Index(index) => j.at_index(*index).into_iter().collect::<Vec<_>>(),
+                            PathPart::Traverse => j.as_array().map(|v| v.iter().collect::<Vec<_>>()).unwrap_or(vec![]),
+                        })
+                        .collect()
+                })
+                .is_empty()
         }
     }
 }
@@ -133,7 +180,12 @@ trait ValueExtInternal {
 }
 
 impl ValueExtInternal for Value {
-    fn modify_part_in_place(&mut self, part: &PathPart, modify: impl Fn(&mut Value) -> (), default: impl Fn() -> Value) {
+    fn modify_part_in_place(
+        &mut self,
+        part: &PathPart,
+        modify: impl Fn(&mut Value) -> (),
+        default: impl Fn() -> Value,
+    ) {
         match part {
             PathPart::Field(name) => match self {
                 Value::Object(jo) => {
@@ -156,11 +208,10 @@ impl ValueExtInternal for Value {
                     if ja.len() <= *idx {
                         let items_to_add = *idx - ja.len() + 1;
                         ja.append(&mut vec![Value::Null; items_to_add]);
-
                     }
 
                     modify(ja.get_mut(*idx).unwrap())
-                },
+                }
                 _ => {
                     let mut new_val = default();
                     modify(&mut new_val);
@@ -168,9 +219,11 @@ impl ValueExtInternal for Value {
                 }
             },
             PathPart::Traverse => match self {
-                Value::Array(ja) => for jv in ja.iter_mut() {
-                    modify(jv);
-                },
+                Value::Array(ja) => {
+                    for jv in ja.iter_mut() {
+                        modify(jv);
+                    }
+                }
                 _ => {
                     let mut new_val = default();
                     modify(&mut new_val);
@@ -184,21 +237,21 @@ impl ValueExtInternal for Value {
         match part {
             PathPart::Field(name) => self.as_object().map(|m| m.contains_key(name)).unwrap_or(false),
             PathPart::Index(idx) => self.as_array().map(|a| a.len() > *idx).unwrap_or(false),
-            PathPart::Traverse => self.is_array()
+            PathPart::Traverse => self.is_array(),
         }
     }
 
     fn field(&self, field_name: &String) -> Option<&Value> {
         match self {
             Value::Object(map) => map.get(field_name),
-            _ => None
+            _ => None,
         }
     }
 
     fn at_index(&self, index: usize) -> Option<&Value> {
         match self {
             Value::Array(vec) => vec.get(index),
-            _ => None
+            _ => None,
         }
     }
 
@@ -217,8 +270,8 @@ impl ValueExtInternal for Value {
 
 #[cfg(test)]
 mod optic_tests {
-    use serde_json::{json, Value};
     use crate::utils::js::optic::{JsonOptic, ValueExt};
+    use serde_json::{json, Value};
 
     #[test]
     fn setter_should_create_fields_recursively_in_empty_json() {
@@ -252,7 +305,10 @@ mod optic_tests {
 
     #[test]
     fn setter_should_keep_array_contents() {
-        let optic = JsonOptic::empty().field("outer".to_string()).field("inner".to_string()).index(2);
+        let optic = JsonOptic::empty()
+            .field("outer".to_string())
+            .field("inner".to_string())
+            .index(2);
         let mut target = json!({"outer": {"inner": [1, 2, 3]}});
 
         target.set(&optic, &Value::from(4));
@@ -261,7 +317,11 @@ mod optic_tests {
 
     #[test]
     fn setter_should_set_fields_inside_arrays() {
-        let optic = JsonOptic::empty().field("outer".to_string()).field("inner".to_string()).index(2).field("v".to_string());
+        let optic = JsonOptic::empty()
+            .field("outer".to_string())
+            .field("inner".to_string())
+            .index(2)
+            .field("v".to_string());
         let mut target = json!({"outer": {"inner": [{"v": 1}, {"v": 2}, {"v": 3}]}});
 
         target.set(&optic, &Value::from(4));
@@ -270,7 +330,10 @@ mod optic_tests {
 
     #[test]
     fn setter_should_write_at_correct_index() {
-        let optic = JsonOptic::empty().field("outer".to_string()).field("inner".to_string()).index(1);
+        let optic = JsonOptic::empty()
+            .field("outer".to_string())
+            .field("inner".to_string())
+            .index(1);
         let mut target = json!({"outer": {"inner": [42]}});
 
         target.set(&optic, &Value::from(100));
@@ -279,15 +342,39 @@ mod optic_tests {
 
     #[test]
     fn setter_should_append_fields_inside_arrays() {
-        let optic1 = JsonOptic::empty().field("inner".to_string()).index(0).field("vv".to_string());
+        let optic1 = JsonOptic::empty()
+            .field("inner".to_string())
+            .index(0)
+            .field("vv".to_string());
         let mut target1 = json!({"inner": [{"v": 1}, {"v": 2}, {"v": 3}]});
         target1.set(&optic1, &Value::from(4));
         assert_eq!(target1, json!({"inner": [{"v": 1, "vv": 4}, {"v": 2}, {"v": 3}]}));
 
-        let optic2 = JsonOptic::empty().field("inner".to_string()).index(1).field("vv".to_string());
+        let optic2 = JsonOptic::empty()
+            .field("inner".to_string())
+            .index(1)
+            .field("vv".to_string());
         let mut target2 = json!({"inner": [{"v": 1}, {"v": 2}, {"v": 3}]});
         target2.set(&optic2, &Value::from(4));
         assert_eq!(target2, json!({"inner": [{"v": 1}, {"v": 2, "vv": 4}, {"v": 3}]}))
+    }
+
+    #[test]
+    fn getter_should_extract_json() {
+        let optic = JsonOptic::empty().field("outer".to_string()).field("inner".to_string());
+        let target = json!({"outer": {"inner": {"a": {"b": "c"}}}});
+
+        let result = target.get_all(&optic);
+        assert_eq!(result, vec![&json!({"a": {"b": "c"}})])
+    }
+
+    #[test]
+    fn getter_should_return_empty_json_if_there_is_no_subtree() {
+        let optic = JsonOptic::empty().field("outer".to_string()).field("inner".to_string());
+        let target = json!({"a": {"b": "c"}});
+
+        let result = target.get_all(&optic);
+        assert!(result.is_empty())
     }
 
     #[test]
