ce2e13c81d386ba74fa8ef3b6a9f52aa8870ca4a
Daniel Slapman <danslapman@gmail.com>
2023-08-13 18:19:05 +0200

    Very basic dummy exec route

---
+++ src/api.rs
@@ -0,0 +1,11 @@
+use rocket::http::{Status, ContentType};
+use std::path::PathBuf;
+
+pub mod model;
+
+#[get("/api/rustybird/exec/<path..>")]
+pub fn exec_get(path: PathBuf) -> (Status, (ContentType, String)) {
+    let path_str = path.into_os_string().into_string().unwrap();
+
+    (Status::Ok, (ContentType::Text, path_str))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #[macro_use] extern crate rocket;
 
+pub mod api;
 pub mod utils;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
 
 //https://github.com/intellij-rust/intellij-rust/issues/5975
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
-    rocket::build().mount("/", routes![index]).launch().await;
+    rocket::build().mount("/", routes![api::exec_get]).launch().await;
 }
