1c827fffdf68fa5df1860c5e2dd48f5ad91274db
Daniel Slapman <danslapman@gmail.com>
2023-08-13 21:34:18 +0200

    guard for QueryParameters

--- src/api.rs
+++ src/api.rs
@@ -6,7 +6,7 @@ pub mod model;
 pub mod guards;
 
 #[get("/api/rustybird/exec/<path..>")]
-pub fn exec_get(path: PathBuf, headers: RequestHeaders<'_>) -> (Status, (ContentType, String)) {
+pub fn exec_get(path: PathBuf, headers: RequestHeaders<'_>, query: QueryParameters<'_>) -> (Status, (ContentType, String)) {
     let path_str = path.into_os_string().into_string().unwrap();
 
     (Status::Ok, (ContentType::Text, path_str))
