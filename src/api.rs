12d22431eb5989379fbeeb57c1dc069048af5bfb
Daniel Slapman <danslapman@gmail.com>
2023-08-13 20:31:45 +0200

    Include RequestHeaders in exec route

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,12 @@
 use rocket::http::{Status, ContentType};
 use std::path::PathBuf;
+use crate::api::guards::*;
 
 pub mod model;
 pub mod guards;
 
 #[get("/api/rustybird/exec/<path..>")]
-pub fn exec_get(path: PathBuf) -> (Status, (ContentType, String)) {
+pub fn exec_get(path: PathBuf, headers: RequestHeaders<'_>) -> (Status, (ContentType, String)) {
     let path_str = path.into_os_string().into_string().unwrap();
 
     (Status::Ok, (ContentType::Text, path_str))
