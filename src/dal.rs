95c87c51ef6abf07fc771c7336c8d7c294ec4d3d
Daniel Slapman <danslapman@gmail.com>
2023-08-30 18:20:19 +0200

    Add StateDao, create_state implementation

--- src/dal.rs
+++ src/dal.rs
@@ -1,8 +1,10 @@
 use crate::error::Error;
 use crate::model::persistent::*;
+use chrono::Utc;
 use diesel::prelude::*;
 use diesel::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
+use serde_json::Value;
 
 pub mod error;
 
@@ -23,6 +25,31 @@ impl StubDao {
 
         let res = diesel::insert_into(stub)
             .values(&new_stub)
+            .execute(&mut conn)?;
+
+        Ok(res)
+    }
+}
+
+#[derive(Clone)]
+pub struct StateDao {
+    pool: Pool<ConnectionManager<PgConnection>>
+}
+
+impl StateDao {
+    pub fn new(pool: Pool<ConnectionManager<PgConnection>>) -> StateDao {
+        StateDao { pool }
+    }
+
+    pub async fn create_state(&self, state_data: Value) -> Result<usize, Error> {
+        use crate::schema::state::dsl::*;
+
+        let mut conn = self.pool.get()?;
+
+        let new_state = NewState { created: Utc::now(), data: state_data };
+
+        let res = diesel::insert_into(state)
+            .values(&new_state)
             .execute(&mut conn)?;
 
         Ok(res)
