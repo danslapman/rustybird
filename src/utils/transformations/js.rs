d3b71441e89f96b7e39f11f99fbc74bc6e476c10
Daniel Slapman <danslapman@gmail.com>
2023-09-17 21:02:20 +0200

    Implement transform_values_in_place

--- src/utils/transformations/js.rs
+++ src/utils/transformations/js.rs
@@ -1,11 +1,15 @@
 use serde_json::Value;
 
 pub trait JsonTransformations {
-    fn transform_values(&mut self, modify: fn(Value) -> Value) -> Self;
+    fn transform_values_in_place(&mut self, modify: fn(&mut Value) -> ());
 }
 
-impl JsonTransformations for &Value {
-    fn transform_values(&mut self, modify: fn(Value) -> Value) -> Self {
-        todo!()
+impl JsonTransformations for Value {
+    fn transform_values_in_place(&mut self, modify: fn(&mut Value) -> ()) {
+        match self {
+            Value::Null | Value::Bool(_) | Value::Number(_) | Value::String(_) => modify(self),
+            Value::Array(vs) => vs.iter_mut().for_each(|el| el.transform_values_in_place(modify)),
+            Value::Object(kvs) => kvs.iter_mut().for_each(|(_, val)| val.transform_values_in_place(modify))
+        }
     }
 }
